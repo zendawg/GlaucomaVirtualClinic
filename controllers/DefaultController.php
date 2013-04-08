3f09f75683f3dd91797695ee696f48044bb87523
zendawg <rich@statefive.org>
2013-04-08 14:25:19 +0100

    Removed create/update since at this time clinics are R/O.

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,14 +2,6 @@
 
 class DefaultController extends BaseEventTypeController {
 
-    public function actionCreate() {
-        parent::actionCreate();
-    }
-
-    public function actionUpdate($id) {
-        parent::actionUpdate($id);
-    }
-
     public function actionView($id) {
         if (!$this->patient = Patient::model()->find('id=:id', array(':id' => $id))) {
             throw new CHttpException(403, 'Invalid patient id.');
